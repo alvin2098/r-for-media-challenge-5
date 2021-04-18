c88661a61e4cc480dfef107fd492df55e409fc64
Your Name <Alvin Aziz>
2021-04-18 18:36:25 +0200

    Die größten Wohnungen befinden sich nicht in den einkommenstärksten Stadtteilen (Lemsahl-Mellingstedt und Wohldorf-Ohlstedt). In den einkommenstärksten Stadtteilen wie Blankenese und Nienstedten gibt es ziemlich teuere und größe Wohnungen.

--- challenge-5.R
+++ challenge-5.R
@@ -1,3 +1,5 @@
+### --- R Challange 5, Alvin Aziz, 18.04.2021 --- ###
+
 # B. Kießling
 # Challenge 5: Visualisierung von Stadtteilsdaten
 # Achtung: Die Challenge hat keinen Code zum überprüfen aber dafür eine Plot als png wie euer Ergebnis aussehen sollte
@@ -25,3 +27,41 @@
 
 # 6. Beschreibe mit einem Satz, was dir bei der Interpretation der Daten auffällt und committe diesen mit deinen Ergebnissen
 
+
+### --- Load libraries --- ###
+library(dplyr)
+library(purrr)
+library(ggplot2)
+library(janitor)
+library(tidyr)
+library(readxl)
+
+### --- Stadtteile Dataset --- ###
+stadtteile <-
+  read_excel("./data/StadtteilprofileBerichtsjahr2017.xlsx", skip = 3) %>% 
+  clean_names() %>% 
+  select(x1, gesamtbetrag_der_einkunfte_je_steuerpflichtigen_lohn_und_einkommen_steuer_im_jahr, durchschnittliche_wohnungsgrosse_in_m2, durchschnittlicher_immobilienpreis_fur_eine_eigentums_wohnung_in_eur_m2) %>% 
+  rename(stadtteil = x1) %>% 
+  rename(einkommen = gesamtbetrag_der_einkunfte_je_steuerpflichtigen_lohn_und_einkommen_steuer_im_jahr) %>% 
+  rename(wohnungsgroesse = durchschnittliche_wohnungsgrosse_in_m2) %>% 
+  rename(kaufpreis_m2 = durchschnittlicher_immobilienpreis_fur_eine_eigentums_wohnung_in_eur_m2)
+
+is.na(stadtteile) <- 
+  stadtteile=="."
+
+stadtteile$kaufpreis_m2 <-
+  as.integer(stadtteile$kaufpreis_m2)
+  
+### --- Plot dataset with dot-plot --- ###
+ggplot(data = stadtteile, aes(x=einkommen, y=wohnungsgroesse, color=kaufpreis_m2, label=stadtteil)) +
+  geom_point() +
+  ggtitle("Vergleich: Wohnfläche und Einkommen in Hamburg") +
+  geom_text(data=subset(stadtteile, einkommen > 112500 | wohnungsgroesse > 125), aes(einkommen,wohnungsgroesse,label=stadtteil), vjust=-1, hjust=1) +
+  ylab("Wohnungsgröße in m2") +
+  xlab("Einkommen in €") +
+  labs(color = "Kaufpreis pro m2 in €") +
+  theme(
+    legend.position = c(.95, .1),
+    legend.justification = c("right", "bottom"),
+    legend.box.just = "right",
+  )
